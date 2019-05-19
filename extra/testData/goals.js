972963c7011462a1d0ea00d068f3f91522e0a1a2
Matthew Mckeller <MattMckeller@gmail.com>
2019-05-18 21:30:51 -0400

    Add test data

---
+++ extra/testData/goals.js
@@ -0,0 +1,40 @@
+import { MY_USER_TEST_DATA } from './users';
+
+export const STUDENT_GOAL_TEST_DATA = [
+  {
+    goalNumber: 1,
+    goal: 'This is a goal 1',
+    createdAt: '01/01/2019',
+    updatedAt: '01/01/2020',
+    student: {},
+    user: MY_USER_TEST_DATA,
+    notes: [],
+  },
+  {
+    goalNumber: 2,
+    goal: 'This is a goal 2 with long text with long text with long text with long text with long text with long text with long text with long text with long text',
+    createdAt: '01/02/2019',
+    updatedAt: '01/02/2020',
+    student: {},
+    user: MY_USER_TEST_DATA,
+    notes: [],
+  },
+  {
+    goalNumber: 3,
+    goal: 'This is a goal 3',
+    createdAt: '01/03/2019',
+    updatedAt: '01/03/2020',
+    student: {},
+    user: MY_USER_TEST_DATA,
+    notes: [],
+  },
+  {
+    goalNumber: 4,
+    goal: 'This is a goal 4',
+    createdAt: '01/04/2019',
+    updatedAt: '01/04/2020',
+    student: {},
+    user: MY_USER_TEST_DATA,
+    notes: [],
+  },
+];
---
+++ extra/testData/users.js
@@ -0,0 +1,29 @@
+
+export const MY_USER_TEST_DATA = {
+  firstName: 'Jim',
+  lastName: 'Carrey',
+  email: 'jimcarrey@gmail.com',
+  phone: '1234567890',
+  schools: [],
+  roles: [],
+};
+
+export const USER_1_TEST_DATA = {
+  firstName: 'Seth',
+  lastName: 'Rogen',
+  email: 'sethrogen@gmail.com',
+  phone: '1234567891',
+  schools: [],
+  roles: [],
+};
+
+export const USER_2_TEST_DATA = {
+  firstName: 'Will',
+  lastName: 'Smith',
+  email: 'willsmith@gmail.com',
+  phone: '1234567892',
+  schools: [],
+  roles: [],
+};
+
+
